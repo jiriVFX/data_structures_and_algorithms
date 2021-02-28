2a647fa39d14ac15a10a6c8e6e8e11cd096fab85
jirin <jirinovy@outlook.com>
2021-02-28 12:01:30 +0000

    Add factorial function using recursion and loop.

---
+++ recursion/factorial_using_recursion_and_loop.py
@@ -0,0 +1,40 @@
+# Find a factorial of any number
+
+# Original recursion function - required a global variable
+# result = 1
+#
+#
+# def factorial_rec_old(number):
+#     global result
+#     if number > 1:
+#         result = result * number
+#         number -= 1
+#         factorial_rec_old(number)
+#     return result
+
+
+# Using a recursion O(n)
+def factorial_rec(number):
+    """Return factorial of an integer using recursion"""
+    if number > 1:
+        return number * factorial_rec(number - 1)
+    return number
+
+
+# Using a while loop O(n)
+def factorial_loop(number):
+    """Return factorial of an integer using a while loop"""
+    if type(number) == int:
+        result = number
+        number -= 1
+        while number > 1:
+            result = result * number
+            number -= 1
+        return result
+    else:
+        return "Invalid type"
+
+
+# print(factorial_rec_old(5))
+print(factorial_rec(5))
+print(factorial_loop(5))
