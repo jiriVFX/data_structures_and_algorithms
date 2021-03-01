d63ee2ff87ae49b91899f14ce47ead590e5cbf80
jirin <jirinovy@outlook.com>
2021-03-01 11:14:04 +0000

    Add recursive reverse a string function.

--- lists_(arrays)/reverse_a_string.py
+++ lists_(arrays)/reverse_a_string.py
@@ -1,15 +1,20 @@
+# Implement functions that reverse a string, one with iteration and the other with recursion.
+# Iterative
 def reverse_a_string(string):
     if type(string) == str and len(string) > 1:
         reversed_string = ""
-        for i in range(1, len(string)+1):
-            reversed_string += string[len(string)-i]
+        for i in range(1, len(string) + 1):
+            reversed_string += string[len(string) - i]
         return reversed_string
     else:
         return "Input is not a valid string longer than 1 char."
 
+
 print(reverse_a_string("Hello, my name is Jirka."))
 
+
 # ----------------------------------------------------------------------------------------------------------------------
+# Iterative 2
 # Easy version (Using slice function [start:stop:step])
 
 def reverse_a_string_easy(string):
@@ -18,4 +23,18 @@ def reverse_a_string_easy(string):
     else:
         return "Input is not a valid string longer than 1 char."
 
+
 print(reverse_a_string_easy("Hello, my name is Jirka."))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Recursive
+
+def reverse_a_string_rec(string):
+    if type(string) == str and len(string) >= 1:
+        return reverse_a_string_rec(string[1:]) + string[0]
+    else:
+        return string
+
+
+print(reverse_a_string_rec("Hello, my name is Jirka."))
