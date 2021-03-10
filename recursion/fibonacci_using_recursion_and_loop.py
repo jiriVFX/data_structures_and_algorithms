d0dc2cbc78e4cab7b665737cb9da2dda021c291b
jirin <jirinovy@outlook.com>
2021-03-10 12:39:39 +0000

    Add dynamic version of recursive Fibonacci function with caching.

--- recursion/fibonacci_using_recursion_and_loop.py
+++ recursion/fibonacci_using_recursion_and_loop.py
@@ -1,5 +1,6 @@
 # Return nth number in Fibonacci sequence, n is given index number
 
+
 # O(2^n) exponential time complexity
 def fibonacci_rec(n):
     """Fibonacci sequence with recursion. Returns nth element in the sequence."""
@@ -19,5 +20,19 @@ def fibonacci_loop(n):
     return fibonacci_list[n]
 
 
+# O(n) - dynamic programming.
+# Caching already computed part of the sequence
+def fibonacci_cached(n, cache):
+    """More efficient recursive Fibonacci sequence with caching. Returns nth element in the sequence."""
+    if n > 1:
+        if n in cache:
+            return cache[n]
+        else:
+            cache[n] = fibonacci_cached(n - 1, cache) + fibonacci_cached(n - 2, cache)
+            return cache[n]
+    return n
+
+
 print(fibonacci_rec(8))
 print(fibonacci_loop(8))
+print(fibonacci_cached(8, cache={0: 0, 1: 1}))
