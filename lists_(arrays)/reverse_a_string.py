3f4d02c321266dfbd698b6c49c9756c96f89e6b3
jirin <jirinovy@outlook.com>
2021-03-14 13:35:26 +0000

    Add reverse a string list inplace function.

--- lists_(arrays)/reverse_a_string.py
+++ lists_(arrays)/reverse_a_string.py
@@ -28,6 +28,26 @@ print(reverse_a_string_easy("Hello, my name is Jirka."))
 
 
 # ----------------------------------------------------------------------------------------------------------------------
+# Iterative 3
+# Reverse a list of characters inplace
+# O(n) time complexity, O(1) space complexity
+
+def reverse_a_string_inplace(string):
+    length = len(string) - 1
+    half = int(length / 2)
+    for i in range(half + 1):
+        temp = string[i]
+        string[i] = string[length - i]
+        string[length - i] = temp
+
+
+string_list = ["H", "e", "l", "l", "o", ",", " ", "m", "y", " ", "n", "a", "m", "e", " ", "i", "s", " ", "J", "i", "r",
+               "k", "a"]
+reverse_a_string_inplace(string_list)
+print(string_list)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
 # Recursive
 
 def reverse_a_string_rec(string):
