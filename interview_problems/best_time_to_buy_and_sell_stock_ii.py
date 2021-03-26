2dc37bc37578ea6a9f6c37cf6e3ac62a2ea4aa1c
jirin <jirinovy@outlook.com>
2021-03-26 23:02:15 +0000

    Add Best Time to Buy and Sell Stock II problem.

--- interview_problems/best_time_to_buy_and_sell_stock_ii.py
+++ interview_problems/best_time_to_buy_and_sell_stock_ii.py
@@ -10,7 +10,6 @@ def max_profit(prices):
     :rtype: int
     """
     total_profit = 0
-
     for i in range(1, len(prices)):
         if prices[i] > prices[i - 1]:
             total_profit += prices[i] - prices[i - 1]
