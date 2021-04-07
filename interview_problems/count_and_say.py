23b21843feb058b8a454785ecf5f3eb63fe8d8f1
jirin <jirinovy@outlook.com>
2021-04-07 21:02:58 +0100

    Improve Count and Say problem solution and complexity description.

--- interview_problems/count_and_say.py
+++ interview_problems/count_and_say.py
@@ -20,7 +20,7 @@
 #  9.     31131211131221
 # 10.     13211311123113112211
 
-# O(n^3) time complexity, O(1) space complexity
+# O(2^n) time complexity, O(2^n) space complexity
 def count_and_say(n):
     """
     :type n: int
@@ -31,35 +31,28 @@ def count_and_say(n):
     last_in_sequence = "1"
 
     for i in range(n - 1):
-        # print(last_in_sequence)
         new_in_sequence = ""
-        digit_counter = {}
+        digit_counter = 0
 
         last_digit = last_in_sequence[0]
 
         # Go through each digit and count it
-        for digit in last_in_sequence:
-            # if current digit is same as the last one
-            if last_digit == digit:
-                if digit in digit_counter:
-                    digit_counter[digit] += 1
-                else:
-                    digit_counter[digit] = 1
+        for digit in range(len(last_in_sequence)):
+            # if the current digit is not the same as the last digit
+            # add the last_digit in the new_in_sequence
+            if last_digit != last_in_sequence[digit]:
+                new_in_sequence += str(digit_counter) + last_digit
+                # Reset the counter
+                digit_counter = 1
             else:
-                # if the current digit is not same as the last digit
-                # write the current digits in the new_in_sequence
-                for key, value in digit_counter.items():
-                    new_in_sequence += str(value) + key
-                # Empty the dictionary
-                digit_counter = {}
-                # Add the current digit to the counter
-                digit_counter[digit] = 1
-            # The current digit is now the last digit for the next iteration
-            last_digit = digit
+                digit_counter += 1
 
-        # write the remaining numbers into current sequence
-        for key, value in digit_counter.items():
-            new_in_sequence += str(value) + key
+            # if digit is the last digit in the current sequence
+            if digit == len(last_in_sequence) - 1:
+                new_in_sequence += str(digit_counter) + last_in_sequence[digit]
+
+            # The current digit is now the last digit for the next iteration
+            last_digit = last_in_sequence[digit]
 
         # replace previous n-1 sequence item with the new item
         last_in_sequence = new_in_sequence
@@ -71,64 +64,3 @@ print(count_and_say(1))
 print(count_and_say(4))
 print(count_and_say(6))
 print(count_and_say(10))
-
-
-# O(n^3) time complexity - again the same problem - different output on LeetCode compared to the local machine
-def count_and_say1_1(n):
-    """
-    :type n: int
-    :rtype: str
-    """
-    if n == 1:
-        return "1"
-    last_in_sequence = "1"
-
-    for i in range(n - 1):
-        # print(last_in_sequence)
-        new_in_sequence = ""
-        digit_counter = {}
-
-        last_digit = last_in_sequence[0]
-
-        # Go through each digit and count it
-        for digit in range(len(last_in_sequence)):
-            # if current digit is the same as the last one and is not the last in last_in_sequence
-            if last_digit == last_in_sequence[digit] and digit < len(last_in_sequence) - 1:
-                if last_in_sequence[digit] in digit_counter:
-                    digit_counter[last_in_sequence[digit]] += 1
-                else:
-                    digit_counter[last_in_sequence[digit]] = 1
-            else:
-                # if the current digit is not same as the last digit
-                # or if the current digit is the last in the last sequence
-                # write the current digits in the new_in_sequence
-
-                if digit == len(last_in_sequence) - 1:
-                    # Add the current digit to the counter
-                    if last_in_sequence[digit] in digit_counter:
-                        digit_counter[last_in_sequence[digit]] += 1
-                    else:
-                        digit_counter[last_in_sequence[digit]] = 1
-
-                for key, value in digit_counter.items():
-                    new_in_sequence += str(value) + key
-                # Empty the dictionary
-                digit_counter = {}
-
-                if last_digit != last_in_sequence[digit]:
-                    # Add the current digit to the counter
-                    digit_counter[last_in_sequence[digit]] = 1
-
-            # The current digit is now the last digit for the next iteration
-            last_digit = last_in_sequence[digit]
-
-        # replace previous n-1 sequence item with the new item
-        last_in_sequence = new_in_sequence
-
-    return last_in_sequence
-
-
-print(count_and_say1_1(1))
-print(count_and_say1_1(4))
-print(count_and_say1_1(6))
-print(count_and_say1_1(10))
