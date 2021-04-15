50192b6b5ab94367b4962e1a7cdcc55bffba2905
jirin <jirinovy@outlook.com>
2021-04-15 12:55:18 +0100

    Add Break Palindrome problem solution.

--- interview_problems/break_palindrome.py
+++ interview_problems/break_palindrome.py
@@ -6,13 +6,13 @@
 # Return "IMPOSSIBLE" if string can't be created
 
 
-def break_palindrome(palindromeStr):
-    if len(palindromeStr) == 1:
+def break_palindrome(palindrome_str):
+    if len(palindrome_str) == 1:
         return "IMPOSSIBLE"
 
     return_palindrome = ""
-    for i in range(len(palindromeStr)):
-        position = ord(palindromeStr[i])
+    for i in range(len(palindrome_str)):
+        position = ord(palindrome_str[i])
         # 97 = "a" in unicode
         if position > 97:
             # In Explanation "abca" is one of possible outputs for "acca", but test fails and expects "aaca" output
@@ -22,7 +22,7 @@ def break_palindrome(palindromeStr):
             new_char = chr(97)
             return_palindrome += new_char
             # add the rest of the string
-            return_palindrome += palindromeStr[i + 1:]
+            return_palindrome += palindrome_str[i + 1:]
             # when changed char is in the middle of the string
             if i == len(return_palindrome) // 2 and (
                     return_palindrome[i] == return_palindrome[i - 1] == return_palindrome[i + 1]):
@@ -30,7 +30,7 @@ def break_palindrome(palindromeStr):
             else:
                 return return_palindrome
         else:
-            return_palindrome += palindromeStr[i]
+            return_palindrome += palindrome_str[i]
 
     return "IMPOSSIBLE"
 
