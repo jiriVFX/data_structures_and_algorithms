3a60fd0354a342a7a623e9a7446d30153f566ab8
jirin <jirinovy@outlook.com>
2021-04-15 12:12:26 +0100

    No KMP working solutions.

--- interview_problems/implement_strstr().py
+++ interview_problems/implement_strstr().py
@@ -5,59 +5,59 @@
 # This is consistent to C's strstr() and Java's indexOf().
 
 # Solution 1 - Naive approach - Time Limit Exceeded on LeetCode
-# O(n^2) time complexity, O(1) space complexity
-def str_str(haystack, needle):
-    """
-    :type haystack: str
-    :type needle: str
-    :rtype: int
-    """
-    # if needle is an empty string
-    if len(needle) == 0:
-        return 0
-    # if needle is longer than haystack
-    elif len(needle) > len(haystack):
-        return -1
-
-    start_index = None
-
-    for i in range(len(haystack)):
-        # Reset start_index at every iteration
-        start_index = None
-        j = i
-        n = 0
-        # check each char of needle in haystack starting from j (j = i at the start)
-        while n < len(needle) and j < len(haystack):
-            # if chars equal, record the start index if it's empty
-            if haystack[j] == needle[n]:
-                if start_index is None:
-                    start_index = j
-                # increment loop variables
-                j += 1
-                n += 1
-            # break the loop if chars don't equal
-            else:
-                break
-
-        # if we found all characters in needle
-        if n == len(needle):
-            return start_index
-
-    # if we get to this point, matching substring wasn't found
-    return -1
-
-
-print(str_str(haystack="hello", needle="ll"))
-print(str_str(haystack="aaaaa", needle="bba"))
-print(str_str(haystack="", needle=""))
-print(str_str(haystack="aaa", needle="aaaa"))
-print(str_str(haystack="mississippi", needle="issipi"))
-print(str_str(haystack="a", needle="a"))
-print(str_str(haystack="abc", needle="c"))
+# O(n * m) time complexity, O(1) space complexity
+# def str_str(haystack, needle):
+#     """
+#     :type haystack: str
+#     :type needle: str
+#     :rtype: int
+#     """
+#     # if needle is an empty string
+#     if len(needle) == 0:
+#         return 0
+#     # if needle is longer than haystack
+#     elif len(needle) > len(haystack):
+#         return -1
+#
+#     start_index = None
+#
+#     for i in range(len(haystack)):
+#         # Reset start_index at every iteration
+#         start_index = None
+#         j = i
+#         n = 0
+#         # check each char of needle in haystack starting from j (j = i at the start)
+#         while n < len(needle) and j < len(haystack):
+#             # if chars equal, record the start index if it's empty
+#             if haystack[j] == needle[n]:
+#                 if start_index is None:
+#                     start_index = j
+#                 # increment loop variables
+#                 j += 1
+#                 n += 1
+#             # break the loop if chars don't equal
+#             else:
+#                 break
+#
+#         # if we found all characters in needle
+#         if n == len(needle):
+#             return start_index
+#
+#     # if we get to this point, matching substring wasn't found
+#     return -1
+#
+#
+# print(str_str(haystack="hello", needle="ll"))
+# print(str_str(haystack="aaaaa", needle="bba"))
+# print(str_str(haystack="", needle=""))
+# print(str_str(haystack="aaa", needle="aaaa"))
+# print(str_str(haystack="mississippi", needle="issipi"))
+# print(str_str(haystack="a", needle="a"))
+# print(str_str(haystack="abc", needle="c"))
 
 
 # # Solution 2 - KMP (Knuth–Morris–Pratt algorithm) - Can't make it work for all test cases
-# # O(n + m) time complexity (length of haystack * length of needle), O(m) space complexity
+# # O(n + m) time complexity (length of haystack + length of needle), O(m) space complexity
 # def str_str2(haystack, needle):
 #     """
 #     :type haystack: str
@@ -130,3 +130,39 @@ print(str_str(haystack="abc", needle="c"))
 # # print(str_str2(haystack="abc", needle="c"))
 # # print(str_str2(haystack="mississippi", needle="pi"))
 # print(str_str2(haystack="babba", needle="bbb"))
+
+# def str_str3(haystack, needle):
+#     # Build the prefix-suffix list - find the longest prefix-suffix in needle
+#     i = 1
+#     j = 0
+#     prefix_suffix = [-1]
+#
+#     while i < len(needle):
+#         if j == -1 or needle[i] == needle[j]:
+#             prefix_suffix.append(i + 1)
+#             i += 1
+#             j += 1
+#         else:
+#             j = prefix_suffix[j]
+#         prefix_suffix.append(0)
+#
+#     print(prefix_suffix)
+#     i = 0
+#     j = 0
+#     while i < len(haystack) and j < len(needle):
+#         if j == -1 or haystack[i] == needle[j]:
+#             i += 1
+#             j += 1
+#         else:
+#             j = prefix_suffix[j]
+#
+#     if j == len(needle):
+#         return i - j
+#     else:
+#         return -1
+
+
+# print(str_str3(haystack="mississippi", needle="issip"))
+# print(str_str3(haystack="mississippi", needle="pi"))
+# print(str_str3(haystack="babba", needle="bbb"))
+# print(str_str3(haystack="hello", needle="ll"))
