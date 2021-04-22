a64bd03017a251a82a9f5a5c6c0abe4eabd26cd4
jirin <jirinovy@outlook.com>
2021-04-22 16:25:39 +0100

    Add Add Two Numbers problem solution.

--- interview_problems/add_two_numbers.py
+++ interview_problems/add_two_numbers.py
@@ -5,12 +5,12 @@
 # You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-# Solution 1
+# Solution 1 - string reversing - easy
 # O(m + n) time complexity, O(n) space complexity
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
@@ -53,3 +53,54 @@ class Solution(object):
 
         return head
 
+
+# Solution 2 - modulo and division operations
+# O(m + n) time complexity, O(n) space complexity
+class Solution2(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        number_1 = 0
+        number_2 = 0
+        result_sum = 0
+
+        # Extract and reverse l1 and l2 -----------------------
+
+        i = 0
+        while l1 is not None:
+            number_1 += (10 ** i) * l1.val
+            l1 = l1.next
+            i += 1
+
+        i = 0
+        while l2 is not None:
+            number_2 += (10 ** i) * l2.val
+            l2 = l2.next
+            i += 1
+
+        # reverse digits in result_sum and add to a new list ---------------------------
+
+        # create dummy head of the result list
+        head = ListNode(-1)
+        tail = head
+
+        result_sum = number_1 + number_2
+
+        if result_sum != 0:
+            while result_sum != 0:
+                digit = result_sum % 10
+                # remove one zero from x
+                result_sum = int(result_sum / 10)
+
+                # create new node and add it to the result list
+                new_node = ListNode(digit)
+                tail.next = new_node
+                tail = new_node
+        else:
+            head.next = ListNode(result_sum)
+
+            # head is dummy, so next is the start of the actual list
+        return head.next
