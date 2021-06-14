215f9daa8dee58c12d2c1cdf9516e477780c3cce
jirin <jirinovy@outlook.com>
2021-06-14 17:34:16 +0100

    Add Min Cost Climbing Stairs optimized problem solution.

--- interview_problems/min_cost_climbing_stairs.py
+++ interview_problems/min_cost_climbing_stairs.py
@@ -4,6 +4,9 @@
 # You can either start from the step with index 0, or the step with index 1.
 # Return the minimum cost to reach the top of the floor.
 
+# Solution 1 - Dynamic Programming (bottom up approach)
+# O(n) time complexity
+# O(n) space complexity
 def minCostClimbingStairs(cost):
     """
     :type cost: List[int]
@@ -23,5 +26,30 @@ def minCostClimbingStairs(cost):
     return min(accumulated_costs[-1], accumulated_costs[-2])
 
 
-print(minCostClimbingStairs([10, 15, 20]))
-print(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+# Solution 2 - Optimized Solution 1
+# O(n) time complexity
+# O(2) space complexity
+def minCostClimbingStairs2(cost):
+    """
+    :type cost: List[int]
+    :rtype: int
+    """
+    # we keep track only of the last two steps' costs
+    accumulated_costs = [0 for _ in range(2)]
+
+    for i in range(len(cost)):
+        if i > 1:
+            current_cost = cost[i] + min(accumulated_costs[0], accumulated_costs[1])
+        else:
+            # no previous costs
+            current_cost = cost[i]
+        # move last cost to first position
+        accumulated_costs[0] = accumulated_costs[1]
+        # add minimum cost at the current step position 1
+        accumulated_costs[1] = current_cost
+    # return minimum of the last two steps (we can choose to start from 0 or 1 - first or second step)
+    return min(accumulated_costs[0], accumulated_costs[1])
+
+
+print(minCostClimbingStairs2([10, 15, 20]))
+print(minCostClimbingStairs2([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
